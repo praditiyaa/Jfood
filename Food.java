dde8bbbe9c7877d105a009329bedf19da36488f3
praditiyaa <muhammad.aditiya71@ui.ac.id>
2020-02-20 17:01:37 +0700

    Module 1

---
+++ Food.java
@@ -0,0 +1,56 @@
+
+/**
+ * Write a description of class Food here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Food
+{
+    // instance variables - replace the example below with your own
+    private int id;
+    private String name;
+    private int price;
+    private String category;
+    /**
+     * Constructor for objects of class Food
+     */
+    public Food()
+    {
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int getId()
+    {
+        return 0;
+    }
+    public String getName()
+    {
+        return "oke";
+    }
+    public int getPrice()
+    {
+        return 0;
+    }
+    public String getCategory()
+    {
+        return "oke";
+    }
+    public void setId(int id)
+    {
+    }
+    public void setName(String name)
+    {
+    }
+    public void setPrice(int price)
+    {
+    }
+    public void setCategory(String category)
+    {
+    }
+}
---
+++ JFood.java
@@ -0,0 +1,29 @@
+
+/**
+ * Write a description of class JFood here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class JFood
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class JFood
+     */
+    public JFood()
+    {
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public void main(String[] args)
+    {
+    }
+}
